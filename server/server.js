1f678f60c33dbf01f3ee0c7d382380f7e332ef0b
johmca <john@mcavenue.co.uk>
2017-06-23 11:37:22 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,20 +21,37 @@ var app = express();
 //work with it as if it was an object
 app.use(bodyParser.json());
 
-//Create routes using .post()
+//Create routes .................
+
+// POST /todos route
+//Use app.post() to register route handler
 //.post() takes 2 args i) URL ii) callback function with args req, res
 app.post('/todos',(req,res)=>{
   //Create todo as an instance of the todo mongoose model
   var todo = new Todo ({
     text: req.body.text
   });
-  //save model to database
+  //save mongoose model to database
   todo.save().then((doc)=>{
     res.send(doc); //Send  doc back to user
   },(e)=>{
       res.status(400).send(e); //Send back status = 400 and send doc back to user
   });
 });
+
+// GET /todos route to return all todo docs
+//Use app.get() to register route handler
+// .get() takes 2 args i) URL ii) callback function with args req, res
+app.get('/todos',(req,res)=>{
+  //Use mongoose .find() method experted from todo model and define promise
+  //callback
+  Todo.find().then((todos)=>{
+    res.send({todos}); //Send the todos back to client as part of an object
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
 
 //Start server
 var port = 3000;
